0f6cea0bdfa81551bd065460fc5a830386b25d6f
Cristian Marcel <lcristianiim@yahoo.com>
2016-04-22 18:34:24 +0300

    Cleaning code

--- modules/module-pattern.js
+++ modules/module-pattern.js
@@ -1,14 +1,17 @@
 var invoiceManager = (function () {
+    // This variables are private
     var self = this;
     var invoices = ['gas invoice'];
 
+    // Return an object exposed to the public
     return {
         getInvoices: function() {
             console.log(invoices);
         },
         addInvoice: function (item) {
             invoices.push(item);
-        }
+        },
+        invoice: invoices
     }
 
 })();
